cb5c68a65c6bb6eb429a1ba01d32a0fbff66e8a7
Vinicius Horewicz <vinicius@horewi.cz>
2013-04-09 15:54:35 -0300

    Symlink gitconfig and gitignore

--- modules/git/init.zsh
+++ modules/git/init.zsh
@@ -16,3 +16,13 @@ pmodload 'helper'
 # Source module files.
 source "${0:h}/alias.zsh"
 
+LINKS=(gitignore gitconfig)
+
+for file in $LINKS
+do
+  if [[ ! -L "$HOME/.$file" ]]
+  then
+    ln -s "${0:h}/${file:t}" $HOME/.$file
+  fi
+done
+
