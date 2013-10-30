b57e10c67172113305361c6da201b3de24415752
Vinicius Horewicz <vinicius@horewi.cz>
2013-10-30 14:18:06 +0100

    nocorrect bundle

--- modules/utility/alias.zsh
+++ modules/utility/alias.zsh
@@ -1,4 +1,5 @@
 alias eb="nocorrect eb"
 alias myip="curl icanhazip.com"
 alias airport="/System/Library/PrivateFrameworks/Apple80211.framework/Versions/Current/Resources/airport"
+alias bundle="nocorrect bundle"
 
