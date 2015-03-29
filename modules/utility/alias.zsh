43ecbee100686f1819d6954a63bab814588dec9e
Vinicius Horewicz <vinicius@horewi.cz>
2015-03-30 00:19:17 +0200

    Update configs

--- modules/utility/alias.zsh
+++ modules/utility/alias.zsh
@@ -1,5 +1,8 @@
-alias eb="nocorrect eb"
 alias myip="curl icanhazip.com"
 alias airport="/System/Library/PrivateFrameworks/Apple80211.framework/Versions/Current/Resources/airport"
 alias bundle="nocorrect bundle"
-
+alias gist="gist --private"
+alias spring="nocorrect spring"
+alias rspec="nocorrect rspec"
+alias cask="brew cask"
+alias cap="nocorrect cap"
